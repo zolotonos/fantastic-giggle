5242319af39174256b9cc2d97e36b44a58b5729f
zolotonos <zolotonos.ua@gmail.com>
2026-05-27 23:47:31 +0300

    фікс флейк лінт

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,24 @@
 import pytest
 from app.main import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
+
 def test_alive(client):
     rv = client.get('/health/alive')
     assert rv.status_code == 200
     assert b'OK' in rv.data
 
+
 def test_root_html_only(client):
     rv = client.get('/', headers={'Accept': 'application/json'})
     assert rv.status_code == 406
+
 
 def test_root_html_accept(client):
     rv = client.get('/', headers={'Accept': 'text/html'})
